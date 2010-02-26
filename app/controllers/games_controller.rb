2a4fb4c617176f2318c790abbbf7ed28465b31e4
Alan Bailward <alan@ufies.org>
2010-02-25 21:53:02 -0800

    Added start of game management.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,13 @@
 class GamesController < ApplicationController
-  def new
+  def show
+    @game = Game.find_by_id(params[:id])
+    @title = "Game at #{@game.gametime}"
   end
   def index
     @games = Game.all
+  end
+  
+  def edit
+    @game = Game.find_by_id(params[id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def title
-    @base_title = "Referee Management"
+    @base_title = "Referee Management Site"
     if @title.nil?
       @base_title
     else
