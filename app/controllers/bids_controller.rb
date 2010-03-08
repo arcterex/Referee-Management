61163086d25d4534b2c5fb68e6231002b8bd0ab4
Alan Bailward <alan@ufies.org>
2010-03-07 19:06:27 -0800

    Create index for bid controller.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,7 @@
 class BidsController < ApplicationController
+  def index
+    # get the user and a list of games that they can bid on
+    # also get a list of their bids so we can put in what 
+    # they've bid on already
+  end
 end
