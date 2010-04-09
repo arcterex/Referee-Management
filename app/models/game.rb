696a02e4ae5516ee9831046f329024ee6376989d
Alan Bailward <alan@ufies.org>
2010-04-08 19:31:24 -0700

    Added alternating row colors

--- app/models/game.rb
+++ app/models/game.rb
@@ -23,13 +23,18 @@ class Game < ActiveRecord::Base
   validates_presence_of :gametime,  :on => :save, :message => "can't be blank"
   validates_presence_of :home,      :on => :save, :message => "can't be blank"
   validates_presence_of :away,      :on => :save, :message => "can't be blank"
+  validates_inclusion_of :gender, :in => [:boy, :girl, :mixed], :on => :create, :message => "extension %s is not included in the list"
 
   # list of games that the user is eligable to bid on
   named_scope :eligible_for, lambda { |user|
     { :conditions => { :age_id => user.age_id } }
   }
   
-  # named_scope :bid_on, lambda { |user|
-  #   { :conditions => }
-  # }
+  # define the gender as a symbol for lookups
+  def gender
+    read_attribute(:gender).to_sym
+  end
+  def gender=(value)
+    write_attribute(:gender, value.to_s)
+  end
 end
